df260922f8e06ddbe9e00f79824ea47f17f3f900
briancherin <brncherin@gmail.com>
2016-11-18 22:16:30 -0500

    Revert "Random number integration"

--- app/src/main/java/joseph/com/githubtest/MainActivity.java
+++ app/src/main/java/joseph/com/githubtest/MainActivity.java
@@ -3,20 +3,12 @@ package joseph.com.githubtest;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
 
-import java.util.Random;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private TextView number;
-    private Button generate;
-    private Random rand;
-    private int n;
+public class MainActivity extends AppCompatActivity {
 
     Button colorButton;
 
@@ -35,21 +27,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 findViewById(R.id.activity_main).setBackgroundColor(color);
             }
         });
-
-        number = (TextView) findViewById(R.id.number);
-        generate = (Button) findViewById(R.id.generate);
-
-        generate.setOnClickListener(this);
-
-        rand = new Random();
-
-
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        n = rand.nextInt(50) + 1;
-        number.setText("" + n);
     }
 }
